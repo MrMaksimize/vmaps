ed208fc242254264f707f3d061cc643a6ce0e4e6
Maksim Pecherskiy <max@maksimize.com>
2016-12-20 19:46:52 -0800

    Starting to play around with sockets.

---
+++ map/index.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/control', function(req, res){
+  res.sendFile(__dirname + '/control.html');
+});
+
+app.get('/fmap', function(req, res){
+  res.sendFile(__dirname + '/fmap.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('connected');
+  socket.on('control_signal', function(data){
+    console.log(data);
+    socket.broadcast.emit('update_map', data)
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+
